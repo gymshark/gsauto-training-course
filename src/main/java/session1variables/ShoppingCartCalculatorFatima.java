5c79cee19a991efa8c01adf4f91e77f888439ea4
fatimafarah <fatima.farah@gymshark.com>
2024-05-22 10:43:39 +0100

    Declared item price and quantity variables and calculated total cost

---
+++ src/main/java/session1variables/ShoppingCartCalculatorFatima.java
@@ -0,0 +1,34 @@
+package session1variables;
+
+public class ShoppingCartCalculatorFatima {
+    public static void main(String[] args) {
+
+        //1.	Declare individual double variables for three item prices.
+        //2.	Declare a double for the sales tax rate. (20% tax – ‘double salesTax = 0.20’)
+        //3.	Use int for quantities of each item, with separate variables.
+        //4.	So 7 variables in total
+
+        double itemOnePrice = 5;
+        double itemTwoPrice = 20;
+        double itemThreePrice = 100;
+
+        double salesTaxRate;
+
+        int itemOneQuantity = 4;
+        int itemTwoQuantity = 20;
+        int itemThreeQuantity = 50;
+
+        //1.	Compute the total cost for each item by multiplying the item price by its quantity.
+        //2.	Sum these totals for a subtotal.
+        //3.	Calculate the tax by applying the sales tax rate to the subtotal.
+        //4.	Compute the grand total by adding the subtotal and the tax.
+
+        double totalCostOfItemOne = itemOnePrice * itemOneQuantity;
+        double totalCostOfItemTwo = itemTwoPrice * itemTwoQuantity;
+        double totalCostOfItemThree = itemThreePrice * itemThreeQuantity;
+
+
+
+
+    }
+}
