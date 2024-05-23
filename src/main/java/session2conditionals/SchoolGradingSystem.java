994d2254db110d26450cc5851efa3b95e9053587
richardpell <richard.pell@gymshark.com>
2024-05-23 11:56:24 +0100

    GradeSchoolSystemRich

---
+++ src/main/java/session2conditionals/SchoolGradingSystem.java
@@ -0,0 +1,29 @@
+package session2conditionals;
+
+public class SchoolGradingSystem {
+
+    public static void main(String[] args) {
+        int = 56;
+
+        if (score <= 0 && score >= 100) {
+            System.out.println("Invalid Score");
+        } else if (score >= 90 && <= 100) {
+                System.out.println("Grade - A");
+            } else if (score >= 80 && score <= 89) {
+                System.out.println("Grade - B");
+            } else if (score >= 70 && score <= 79) {
+                System.out.println("Grade - C");
+            } else if (score >= 60 && score <= 69) {
+                System.out.println("Grade - D");
+            } else if (score < 60 && score >= 0) {
+                System.out.println("Grade - F");
+
+        }
+
+    System.out.println((score);
+
+    }
+
+    }
+    }
+}
