fbcf4f36316a257198e91b165796e0efa2b9e0ad
KodzGS <k.johnson@gymshark.com>
2024-05-23 12:38:12 +0100

    Commit school grading task with score adjustment

--- src/main/java/session2conditionals/SchoolGradingSystemKodi.java
+++ src/main/java/session2conditionals/SchoolGradingSystemKodi.java
@@ -3,17 +3,17 @@ package session2conditionals;
 public class SchoolGradingSystemKodi {
 
   public static void main(String[] args) {
-    int score = 101;
+    int score = 80;
 
     if (score <= 0 || score > 100) {
       System.out.println("Invalid score");
     } else if (score >= 90 && score <= 100) {
       System.out.println("Grade A");
-    } else if (score >= 80 && score < 89) {
+    } else if (score >= 80 && score <= 89) {
       System.out.println("Grade B");
-    } else if (score >= 70 && score < 79) {
+    } else if (score >= 70 && score <= 79) {
       System.out.println("Grade C");
-    } else if (score >= 60 && score < 69) {
+    } else if (score >= 60 && score <= 69) {
       System.out.println("Grade D");
     } else if (score < 60) {
       System.out.println("Below E");
