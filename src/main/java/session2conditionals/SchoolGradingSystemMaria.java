d3fb9d652f1b59b12bc68b5cef445c3f8506fa2e
mariaprabhakar <mariainform@gmail.com>
2024-05-23 10:23:18 +0100

    School Grading System task change in logic

--- src/main/java/session2conditionals/SchoolGradingSystemMaria.java
+++ src/main/java/session2conditionals/SchoolGradingSystemMaria.java
@@ -5,23 +5,24 @@ public class SchoolGradingSystemMaria {
 
 // Hardcode student score
 
-        int score = 56;
-//Determine grades based on scoring range
-        if (score >= 90 && score <= 100) {
-            System.out.println("Grade - A ");
-        } else if (score >= 80 && score <= 89) {
-            System.out.println("Grade - B ");
-        } else if (score >= 70 && score <= 79) {
-            System.out.println("Grade - C ");
-        } else if (score >= 60 && score <= 69) {
-            System.out.println("Grade - D ");
-        } else if (score < 60 && score >= 0) {
-            System.out.println("Grade - F");
-        }
-        // Validation to ensure the score is within a 0-100 range.
+        int score = 0;
 
-        if (score <= 0 || score >= 100) {
-            throw new IndexOutOfBoundsException(" Invalid Scoring range , Please enter a score between 0 and 100");
+//Determine grades based on scoring range
+
+        if (score >= 0 || score <= 100) {
+            if (score >= 90 && score <= 100) {
+                System.out.println("Grade - A ");
+            } else if (score >= 80 && score <= 89) {
+                System.out.println("Grade - B ");
+            } else if (score >= 70 && score <= 79) {
+                System.out.println("Grade - C ");
+            } else if (score >= 60 && score <= 69) {
+                System.out.println("Grade - D ");
+            } else if (score < 60 && score >= 0) {
+                System.out.println("Grade - F");
+            } else
+                // Validation to ensure the score is within a 0-100 range.
+                throw new IndexOutOfBoundsException(" Invalid Scoring range , Please enter a score between 0 and 100");
         }
     }
 }
