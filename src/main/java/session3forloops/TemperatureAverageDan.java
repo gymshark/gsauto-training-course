b5f92dac3abfd113ac9c2ca068d87c58666365e2
danielwilliams <daniel.williams@gymshark.com>
2024-05-28 09:13:40 +0100

    Initial commit

--- src/main/java/session3forloops/TemperatureAverageDan.java
+++ src/main/java/session3forloops/TemperatureAverageDan.java
@@ -3,9 +3,7 @@ package session3forloops;
 public class TemperatureAverageDan {
 
   public static void main(String[] args) {
-
     int[] temperatures = {25, 24, 25, 23, 22, 22, 23, 27, 28, 29};
     int sum = 0;
-
   }
 }
