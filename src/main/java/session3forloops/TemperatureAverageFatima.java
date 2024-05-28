08546c4eb26eef822478566e2bb51cc629949ab0
fatimafarah <fatima.farah@gymshark.com>
2024-05-28 14:21:06 +0100

    created for-each loop and have iterated the array.

---
+++ src/main/java/session3forloops/TemperatureAverageFatima.java
@@ -0,0 +1,21 @@
+package session3forloops;
+
+public class TemperatureAverageFatima {
+    public static void main(String[] args) {
+        //1.	Declare and Initialize an Array:
+        //1.	Create an array to store daily temperatures.
+        //2.	For-Each Loop:
+        //1.	Use a for-each loop to iterate over the array and sum the temperatures.
+        //3.	Calculate Average:
+        //1.	After summing the temperatures, calculate the average.
+        //4.	Output the Result:
+        //1.	Print the average temperature.
+
+       int[] dailyTemperatures= {16,15,18,25,24,5};
+
+       for (int dailyTemperature: dailyTemperatures) {
+           System.out.println(dailyTemperature);
+
+       }
+    }
+}
