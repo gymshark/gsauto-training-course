fc5b2bf6659a820cd0d7549097aa81d9893ed677
KodzGS <k.johnson@gymshark.com>
2024-05-29 22:35:37 +0100

    Commit GradeCalculatorKodi

--- src/main/java/session4methods/GradeCalculatorKodi.java
+++ src/main/java/session4methods/GradeCalculatorKodi.java
@@ -2,24 +2,20 @@ package session4methods;
 
 public class GradeCalculatorKodi {
 
-
-  public static double calculateAverageGrade(int[] grades)
-  {
+  public static double calculateAverageGrade(int[] grades) {
 
     double total = 0;
     for (double grade : grades) {
       total += grade;
     }
-
     return total / grades.length;
   }
 
   public static void main(String[] args) {
 
-   int[] studentGrades = {100, 89, 98, 55, 90, 45, 75, 64, 59};
-   double averageGrade = calculateAverageGrade(studentGrades);
+    int[] studentGrades = {100, 89, 98, 55, 90, 45, 75, 64, 59};
+    double averageGrade = calculateAverageGrade(studentGrades);
 
     System.out.println("Average grade is: " + averageGrade);
   }
-
 }
