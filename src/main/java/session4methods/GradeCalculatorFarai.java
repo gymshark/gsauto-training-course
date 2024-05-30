d4599abaea6f8a28c716f95d4a723d700454aa78
faraikumire <farai.kumire@gymshark.com>
2024-05-30 09:20:27 +0100

    Removed redundant variable and brought the return for the method inline as per PR comment. Formatted class using codeStylexml

--- src/main/java/session4methods/GradeCalculatorFarai.java
+++ src/main/java/session4methods/GradeCalculatorFarai.java
@@ -2,24 +2,22 @@ package session4methods;
 
 public class GradeCalculatorFarai {
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-        int[] studentGrades={59, 93, 57, 38, 69};
+    int[] studentGrades = {59, 93, 57, 38, 69};
 
-        System.out.println("The average grade is " + calculateAverageGrade(studentGrades));
+    System.out.println("The average grade is " + calculateAverageGrade(studentGrades));
+  }
+
+  public static double calculateAverageGrade(int[] studentGrades) {
+
+    int sum = 0;
+
+    for (int studentGrade : studentGrades) {
+
+      sum += studentGrade;
     }
 
-    public static double calculateAverageGrade(int[] studentGrades){
-
-        int sum =0;
-
-        for(int studentGrade:studentGrades){
-
-            sum+=studentGrade;
-        }
-
-       double averageGrade = (double) sum/studentGrades.length;
-
-        return averageGrade;
-    }
+    return (double) sum / studentGrades.length;
+  }
 }
