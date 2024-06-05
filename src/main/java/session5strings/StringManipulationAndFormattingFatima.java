80c1f9a921e072e4c8e5aa28f4b23b26001e78fc
fatimafarah <fatima.farah@gymshark.com>
2024-06-05 11:31:15 +0100

    Started on string formatting using the String Builder method.

--- src/main/java/session5strings/StringManipulationAndFormattingFatima.java
+++ src/main/java/session5strings/StringManipulationAndFormattingFatima.java
@@ -1,5 +1,7 @@
 package session5strings;
 
+import java.util.function.ToDoubleBiFunction;
+
 public class StringManipulationAndFormattingFatima {
 
 
@@ -10,17 +12,35 @@ public class StringManipulationAndFormattingFatima {
         String emailAddress = "fatima.farah@outlook.com";
         String name = "Fatima Farah";
 
-        System.out.println("The username is " + emailAddress.substring(0,12));
-        System.out.println("The index of h is " + emailAddress.substring(0,12).indexOf('h'));
+        //Methods
+        System.out.println("The username is " + emailAddress.substring(0, 12));
+        System.out.println("The index of h is " + emailAddress.substring(0, 12).indexOf('h'));
         System.out.println("The username in uppercase is " + emailAddress.toUpperCase());
 
+        //Equality
         System.out.println("Are the username and name the same: " + emailAddress.replaceAll("\\.", " ")
-                .substring(0,12).equalsIgnoreCase(name));
+                .substring(0, 12).equalsIgnoreCase(name));
+
+        //String Builder
+        StringBuilder message = new StringBuilder();
+        message.append("The original email is ");
+        message.append(emailAddress + " ");
+        message.append("the extracted username is ");
+        message.append(emailAddress.substring(0, 12) + " ");
+        message.append("the uppercase name is ");
+        message.append(name.toUpperCase() + " ");
+        message.append("Is the username and name equal? ");
+        message.append(emailAddress.replaceAll("\\.", " ").substring(0, 12).equalsIgnoreCase(name));
+
+        System.out.println(message);
 
 
+        String.format("The original email is %s, the extracted username is %s, the uppercase name is %s, " +
+                "Is the username and name equal? %b ", emailAddress, emailAddress, name, emailAddress);
 
     }
 
+    //TODO FIX STRING BUILDER
 
     //Use StringBuilder:
     //Create a formatted message that summarizes the results using StringBuilder.
