11382819d75e2d5adee3d134f968b4c75258dae9
fatimafarah <fatima.farah@gymshark.com>
2024-06-10 10:20:45 +0100

    Moved code into the internal class

--- src/main/java/session6classes/LibraryBookFatima.java
+++ src/main/java/session6classes/LibraryBookFatima.java
@@ -1,34 +1,31 @@
 package session6classes;
 
 public class LibraryBookFatima {
+    private String bookTitle;
+    private String author;
+    private int yearOfPublication;
 
-    public static class Book {
-        private String bookTitle;
-        private String author;
-        private int yearOfPublication;
+    public LibraryBookFatima(String bookTitle, String author, int yearOfPublication) {
+        this.bookTitle = bookTitle;
+        this.author = author;
+        this.yearOfPublication = yearOfPublication;
+    }
 
+    public int getYearOfPublication() {
+        return yearOfPublication;
+    }
 
-        public Book(String bookTitle, String author, int yearOfPublication) {
-            this.bookTitle = bookTitle;
-            this.author = author;
-            this.yearOfPublication = yearOfPublication;
-        }
-        public int getYearOfPublication() {
-            return yearOfPublication;
-        }
+    public void setYearOfPublication(int yearOfPublication) {
+        this.yearOfPublication = yearOfPublication;
+    }
 
-        public void setYearOfPublication(int yearOfPublication) {
-            this.yearOfPublication = yearOfPublication;
-        }
-
-        public void bookDetails() {
-            System.out.println("Book title:" + bookTitle + " Author:" + author + " Year: " + getYearOfPublication());
-        }
+    public void bookDetails() {
+        System.out.println("Book title:" + bookTitle + " Author:" + author + " Year: " + getYearOfPublication());
     }
 
     public static void main(String[] args) {
-        Book book1 = new Book("Diaspora Diaries", " Fatima", 2002);
-        Book book2 = new Book("The Cat Sat On The Mat", "Unknown", 2020);
+        LibraryBookFatima book1 = new LibraryBookFatima("Diaspora Diaries", " Fatima", 2002);
+        LibraryBookFatima book2 = new LibraryBookFatima("The Cat Sat On The Mat", "Unknown", 2020);
 
         book1.setYearOfPublication(20300);
         book2.setYearOfPublication(1993);
@@ -36,4 +33,5 @@ public class LibraryBookFatima {
         book1.bookDetails();
         book2.bookDetails();
     }
+
 }
