a6ac53a9def8b8a1bd1b0579c0dbbcb1e95c999e
faraikumire <farai.kumire@gymshark.com>
2024-06-11 15:24:19 +0100

    A Java class to represent a rectangle, using fields to store the dimensions and constructors to initialize these fields.

---
+++ src/main/java/session7fieldsconstructors/Rectangle.java
@@ -0,0 +1,53 @@
+package session7fieldsconstructors;
+
+public class Rectangle {
+
+  private double length;
+  private double width;
+
+  public Rectangle() {
+    this.length = 1.0;
+    this.width = 1.0;
+  }
+
+  public Rectangle(double length, double width) {
+    this.length = length;
+    this.width = width;
+  }
+
+  public double getLength() {
+    return length;
+  }
+
+  public double getWidth() {
+    return width;
+  }
+
+  public double calculateArea(double length, double width) {
+    return length * width;
+  }
+
+  public double calculateParameter(double length, double width) {
+    return (2 * (length + width));
+  }
+
+  public void rectangleDetails() {
+    System.out.println("The length: " + getLength());
+    System.out.println("The width: " + getWidth());
+    System.out.printf("The area: " + calculateArea(getLength(), getWidth()) + "%n");
+    System.out.printf("The perimeter: " + calculateParameter(getLength(), getWidth()));
+  }
+
+
+  public static void main(String[] args) {
+
+    Rectangle defaultRectangle = new Rectangle();
+    Rectangle parameterisedRectangle = new Rectangle(2.2, 3.2);
+
+    System.out.println("Default rectangle details: \n");
+    defaultRectangle.rectangleDetails();
+
+    System.out.println("Parameterised rectangle details: \n");
+    parameterisedRectangle.rectangleDetails();
+  }
+}
