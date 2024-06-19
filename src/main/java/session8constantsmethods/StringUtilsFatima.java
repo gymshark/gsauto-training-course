b1e5e4a0f8e6a392e023c92569e1305321a5b3af
fatimafarah <fatima.farah@gymshark.com>
2024-06-19 12:31:22 +0100

    Put default string into upper case

--- src/main/java/session8constantsmethods/StringUtilsFatima.java
+++ src/main/java/session8constantsmethods/StringUtilsFatima.java
@@ -2,17 +2,17 @@ package session8constantsmethods;
 
 public class StringUtilsFatima {
 
-    public static final String defaultString = "Hello World";
+    public static final String DEFAULT_STRING = "Hello World";
 
     public static String upperCase() {
-        return defaultString.toUpperCase();
+        return DEFAULT_STRING.toUpperCase();
     }
 
     public static int countVowel() {
         int vowelCount = 0;
-        for (int i = 0; i < defaultString.length(); i++) {
-            if (defaultString.charAt(i) == 'a' || defaultString.charAt(i) == 'e' || defaultString.charAt(i) == 'i' ||
-                    defaultString.charAt(i) == 'o' || defaultString.charAt(i) == 'u') {
+        for (int i = 0; i < DEFAULT_STRING.length(); i++) {
+            if (DEFAULT_STRING.charAt(i) == 'a' || DEFAULT_STRING.charAt(i) == 'e' || DEFAULT_STRING.charAt(i) == 'i' ||
+                    DEFAULT_STRING.charAt(i) == 'o' || DEFAULT_STRING.charAt(i) == 'u') {
                 vowelCount += 1;
             }
         }
