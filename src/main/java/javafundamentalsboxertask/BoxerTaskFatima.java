b01ca9203e0f3429af62c81eda12a7f639f7309c
fatimafarah <fatima.farah@gymshark.com>
2024-06-25 16:57:42 +0100

    Working through Boxer task

--- src/main/java/javafundamentalsboxertask/BoxerTaskFatima.java
+++ src/main/java/javafundamentalsboxertask/BoxerTaskFatima.java
@@ -1,15 +1,8 @@
 package javafundamentalsboxertask;
+
 import java.util.Random;
 
 public class BoxerTaskFatima {
-
-    //Player has 100 punches, each with a randomly assigned punch strength between 1 - 100
-    //Every time a number that's a multiple of 5, a punch connects
-    //Every time a number is 90 or above, it's a double strength punch
-    //Each punch can hit up to the initial limit of 100 strength (before doubling)
-    //The boxer you are facing has 1000 health points
-    //Can you knock the boxer out with 100 punches?
-
 
     public static void main(String[] args) {
 
@@ -19,26 +12,24 @@ public class BoxerTaskFatima {
         Random random = new Random();
 
         for (int i = 0; i < punches.length; i++) {
-            random.nextInt();
-            Math.subtractExact(boxerHealthPoints, random.nextInt());
-
+            random.nextInt(100);
             if (random.nextInt() % 5 == 0) {
                 System.out.println("Punch Landed!");
-            } else if (random.nextInt() >= 90) {
+                boxerHealthPoints -= random.nextInt();
+            } else if (random.nextInt() >= 90 || random.nextInt() <100) {
                 System.out.println("Haymaker Landed!");
+                boxerHealthPoints -= random.nextInt();
             } else System.out.println("Punch Missed!");
-
         }
 
-        if (boxerHealthPoints <= 0) {
+        if (boxerHealthPoints > 0) {
+            System.out.println("He's still standing after 100 punches!");
+        } else if (boxerHealthPoints == 0) {
             System.out.println("Knockout");
-        } else if (boxerHealthPoints > 0) {
-            System.out.println("He's still standing!");
-            System.out.println(boxerHealthPoints);
+            System.exit(0);
         }
     }
 }
-
 
 
 
