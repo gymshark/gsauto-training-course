774e89549ba4f404d874a73d8fa1cff10805e02f
fatimafarah <fatima.farah@gymshark.com>
2024-07-01 16:38:36 +0100

    Made changes stated in the PR review.

--- src/main/java/javafundamentalsboxertask/BoxerTaskFatima.java
+++ src/main/java/javafundamentalsboxertask/BoxerTaskFatima.java
@@ -3,36 +3,43 @@ package javafundamentalsboxertask;
 import java.util.Random;
 
 public class BoxerTaskFatima {
-
     public static void main(String[] args) {
 
         Random random = new Random();
         int[] playerPunches = new int[100];
         int boxerHealthPoints = 1000;
-        boolean hasPunchLanded;
 
-        for (int playerPunch : playerPunches) {
+        for (int i = 0; i < playerPunches.length; i++) {
+            playerPunches[i] = random.nextInt(100) + 1;
+            int punchStrength = playerPunches[i];
             System.out.println("Punch Strength:" + random.nextInt(100));
-            if ((random.nextInt() % 5) == 0) {
-                System.out.println("Punch Landed!");
-                hasPunchLanded = true;
-                boxerHealthPoints -= random.nextInt();
-            } else if (random.nextInt() >= 90 || ((random.nextInt() < 100)) && (random.nextInt() >= 90)) {
-                System.out.println("Haymaker Landed!");
-                boxerHealthPoints -= random.nextInt();
-            } else System.out.println("Punch Missed!");
+
+            if (punchStrength % 5 == 0) {
+                if (punchStrength >= 90) {
+                    System.out.println("Haymaker Landed!");
+                    boxerHealthPoints -= punchStrength * 2;
+                    System.out.println("Damage: " + (punchStrength * 2) + " | Boxer Health: " + boxerHealthPoints);
+                } else {
+                    System.out.println("Punch Landed!");
+                    boxerHealthPoints -= punchStrength;
+                    System.out.println("Damage: " + punchStrength + " | Boxer Health: " + boxerHealthPoints);
+                }
+            } else {
+                System.out.println("Punch Missed!");
+            }
+
+            if (boxerHealthPoints > 0) {
+                System.out.println("He's still standing after 100 punches!");
+            } else if ((boxerHealthPoints == 0) || (boxerHealthPoints < 0)) {
+                System.out.println("Knockout");
+                break;
+            }
         }
 
-        if (boxerHealthPoints > 0) {
-            System.out.println("He's still standing after 100 punches!");
-        } else if (boxerHealthPoints == 0) {
-            System.out.println("Knockout");
-            System.exit(0);
-        }
+
 
     }
 }
-
 
 
 
