2016a4b40ee4db9a6dceb141162a9d547dc17b55
KodzGS <k.johnson@gymshark.com>
2024-07-02 15:41:53 +0100

    Commit ElectronicDeviceTask Kodi

---
+++ src/main/java/session10overridingandoverloading/interfacetaskkodi/Fridge.java
@@ -0,0 +1,32 @@
+package session10overridingandoverloading.interfacetaskkodi;
+
+public class Fridge implements ElectronicDevice {
+
+  private boolean powerOn = false;
+
+ @Override
+  public void turnOn() {
+   if (!powerOn) {
+     powerOn = true;
+     System.out.println("fridge is now ON.");
+   } else {
+     System.out.println("fridge was already ON.");
+   }
+ }
+
+  @Override
+  public void turnOff() {
+    if (powerOn) {
+      powerOn = false;
+      System.out.println("fridge is now OFF.");
+    }
+    else {
+      System.out.println("fridge was already OFF.");
+    }
+  }
+
+  @Override
+  public boolean isOn() {
+    return powerOn;
+  }
+}
