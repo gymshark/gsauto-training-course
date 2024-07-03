1bc3e6f5f3fcefba558eeba9ab05f9170824fc64
KodzGS <k.johnson@gymshark.com>
2024-07-03 12:04:02 +0100

    Commit NotificationSystemKodi task

---
+++ src/main/java/session10overridingandoverloading/notificationsystemkodi/EmailNotification.java
@@ -0,0 +1,37 @@
+package session10overridingandoverloading.notificationsystemkodi;
+
+public class EmailNotification extends Notification {
+
+  @Override
+  public void displayNotification() {
+    System.out.println("Email notifications");
+  }
+
+  @Override
+  public void displayNotification(String message) {
+    System.out.println("Email notification: " + message);
+  }
+
+  @Override
+  public void displayNotification(String message, String image) {
+    System.out.println("Email notification: " + message);
+    System.out.println("Image notification: " + image);
+  }
+
+  @Override
+  public void displayNotification(String name, String message, String image) {
+    System.out.println("Email notification: " + name);
+    System.out.println("Email notification: " + message);
+    System.out.println("Image notification: " + image);
+  }
+
+  public void isEmailRead(boolean read) {
+    if (!read) {
+      System.out.println("** Email has not been read :( **");
+    } else System.out.println("** Email has been read :) **");
+  }
+
+  public void deleteEmail() {
+    System.out.println("Email notification deleted");
+  }
+}
---
+++ src/main/java/session10overridingandoverloading/notificationsystemkodi/Notification.java
@@ -0,0 +1,23 @@
+package session10overridingandoverloading.notificationsystemkodi;
+
+public class Notification {
+
+  public void displayNotification() {
+    System.out.println("Notifications");
+  }
+
+  public void displayNotification(String message) {
+    System.out.println("Notification: " + message);
+  }
+
+  public void displayNotification(String message, String image) {
+    System.out.println("Notification: " + message);
+    System.out.println("Image Link: " + image);
+  }
+
+  public void displayNotification(String name, String message, String image) {
+    System.out.println("Name: " + name);
+    System.out.println("Notification: " + message);
+    System.out.println("Image Link: " + image);
+  }
+}
