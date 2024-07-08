6138194e510036a888b4abdc53ebacf3d468769e
KodzGS <k.johnson@gymshark.com>
2024-07-08 11:06:17 +0100

    Commit enumtask kodi

---
+++ src/main/java/session13enums/examples/kodi/Direction.java
@@ -0,0 +1,21 @@
+package session13enums.examples.kodi;
+
+public enum Direction {
+
+  NORTH("North Side is the best"),
+  SOUTH("South Side is the best"),
+  EAST("East Side is the best"),
+  WEST("West Side is the best");
+
+  public final String description;
+
+
+  Direction(String description) {
+    this.description = description;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+}
