4f3cb61e7386524687d0b5846d3e52ea85fc6d67
owyndwight <owyn.dwight@gymshark.com>
2024-07-09 13:05:35 +0100

    Examples

---
+++ src/main/java/session14bddgherkin/examples/Example.feature
@@ -0,0 +1,6 @@
+Feature: Example
+
+  Scenario: Test Example
+    Given the user is on the login page
+    When logging in
+    Then the user should be logged in
---
+++ src/main/java/session15locators/examples/Locators.java
@@ -0,0 +1,5 @@
+package session15locators.examples;
+
+public class Locators {
+  private static final String ADD_TO_BAG_BUTTON = "plp-quickAdd-6714680541387-open";
+}
