d67c544e30bfc4ee81d384486c7ae3f937b8872a
mariaprabhakar <mariainform@gmail.com>
2024-07-10 12:55:16 +0100

    Selenium Locators Task commit

---
+++ src/main/java/session15locators/examples/SeleniumLocators_Maria/Homepage.java
@@ -0,0 +1,9 @@
+package session15locators.examples.SeleniumLocators_Maria;
+
+public class Homepage {
+
+   private static final String GS_LOGO_ICON = "header-logo-select";
+   private static final String SEARCH_CTA= "search-searchTrigger-select";
+   private static final String BAG_CTA = "header-miniBag-select";
+
+}
