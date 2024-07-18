f2d32e784fa563c186d288c70af99a2694026026
fatimafarah <fatima.farah@gymshark.com>
2024-07-18 11:17:28 +0100

    Completed selenium exercise and removed all the web element code. I also added dependencies for selenium in the pom.

--- src/main/java/session15locators/tasks/fatima/ProductDisplayPage.java
+++ src/main/java/session15locators/tasks/fatima/ProductDisplayPage.java
@@ -2,30 +2,19 @@ package session15locators.tasks.fatima;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 
 public class ProductDisplayPage {
 
     static WebDriver driver;
 
-    static WebElement pdpAddToWishlistCTALocator = driver.findElement(By.id("pdp-addToWishlist-add"));
-    static WebElement pdpAddToBagCTALocator = driver.findElement(By.id("pdp-addToBag-submit"));
-    static WebElement pdpSizeXSLocator = driver.findElement(By.id("pdp-size-xs-select"));
-    static WebElement pdpSizeSLocator = driver.findElement(By.id("pdp-size-s-select"));
-    static WebElement pdpSizeMLocator = driver.findElement(By.id("pdp-size-m-select"));
-    static WebElement pdpSizeLLocator = driver.findElement(By.id("pdp-size-l-select"));
-    static WebElement pdpSizeXLLocator = driver.findElement(By.id("pdp-size-xl-select"));
-    static WebElement pdpSizeXXLLocator = driver.findElement(By.id("pdp-size-xxl-select"));
-
-
-    public static String PDP_ADD_TO_WISHLIST_CTA = String.valueOf(pdpAddToWishlistCTALocator);
-    public static String PDP_ADD_TO_BAG_CTA = String.valueOf(pdpAddToBagCTALocator);
-    public static String PDP_SIZE_XS = String.valueOf(pdpSizeXSLocator);
-    public static String PDP_SIZE_S = String.valueOf(pdpSizeSLocator);
-    public static String PDP_SIZE_M = String.valueOf(pdpSizeMLocator);
-    public static String PDP_SIZE_L = String.valueOf(pdpSizeLLocator);
-    public static String PDP_SIZE_XL = String.valueOf(pdpSizeXLLocator);
-    public static String PDP_SIZE_XXL = String.valueOf(pdpSizeXXLLocator);
+    public static String PDP_ADD_TO_WISHLIST_CTA = String.valueOf(driver.findElement(By.id("pdp-addToWishlist-add")));
+    public static String PDP_ADD_TO_BAG_CTA = String.valueOf(driver.findElement(By.id("pdp-addToBag-submit")));
+    public static String PDP_SIZE_XS = String.valueOf(driver.findElement(By.id("pdp-size-xs-select")));
+    public static String PDP_SIZE_S = String.valueOf(driver.findElement(By.id("pdp-size-s-select")));
+    public static String PDP_SIZE_M = String.valueOf(driver.findElement(By.id("pdp-size-m-select")));
+    public static String PDP_SIZE_L = String.valueOf(driver.findElement(By.id("pdp-size-l-select")));
+    public static String PDP_SIZE_XL = String.valueOf(driver.findElement(By.id("pdp-size-xl-select")));
+    public static String PDP_SIZE_XXL = String.valueOf(driver.findElement(By.id("pdp-size-xxl-select")));
 
 }
 
