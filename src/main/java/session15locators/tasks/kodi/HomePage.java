e8568326b4bf5e2039c20ef7903c22a13081050c
KodzGS <k.johnson@gymshark.com>
2024-08-01 15:11:42 +0100

    Commit locators task

---
+++ src/main/java/session15locators/tasks/kodi/HomePage.java
@@ -0,0 +1,9 @@
+package session15locators.tasks.kodi;
+
+public class HomePage {
+
+  public static final String GS_LOGO_ICON = "header-logo-select";
+  public static final String SEARCH_CTA = "search-searchTrigger-select";
+  public static final String BAG_CTA = "header-miniBag-select";
+
+}
---
+++ src/main/java/session15locators/tasks/kodi/ProductDisplayPage.java
@@ -0,0 +1,15 @@
+package session15locators.tasks.kodi;
+
+public class ProductDisplayPage {
+
+  public static final String SIZE_XS_CTA = "pdp-size-xs-select";
+  public static final String SIZE_S_CTA = "pdp-size-s-select";
+  public static final String SIZE_M_CTA = "pdp-size-m-select";
+  public static final String SIZE_L_CTA = "pdp-size-l-select";
+  public static final String SIZE_XL_CTA = "pdp-size-xl-select";
+  public static final String SIZE_XXL_CTA = "pdp-size-xxl-select";
+  public static final String SIZE_3XL_CTA = "pdp-size-3xl-select";
+  public static final String ADD_TO_BAG_CTA = "pdp-addToBag-submit";
+  public static final String ADD_TO_WISHLIST_CTA = "pdp-addToWishlist-add";
+
+}
