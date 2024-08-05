4be382d65db02c144a1056606312276e837bdff1
maryamadnan <maryam.adnan@gymshark.com>
2024-08-05 09:04:37 +0100

    locators task

---
+++ src/main/java/javaclassesandinterfacestask/ClassesAndInterfaceTask_Maryam/Book.java
@@ -0,0 +1,73 @@
+package javaclassesandinterfacestask.ClassesAndInterfaceTask_Maryam;
+
+public class Book {
+
+    private String title;
+    private String author;
+    private String isbn;
+    private boolean isAvailable;
+
+    // Constructor
+
+    public Book (String title, String author, String isbn) {
+
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.isAvailable = true ;
+
+    }
+
+    // getters and setters
+
+    public String getTitle () {
+        return  title;
+    }
+
+    public void setTitle (String title) {
+        this.title = title;
+    }
+
+    public String getAuthor () {
+        return author;
+    }
+    public void setAuthor (String author){
+     this.author = author;
+    }
+
+    public String getIsbn (){
+        return isbn;
+    }
+
+    public void setIsbn (String isbn) {
+        this.isbn = isbn;
+    }
+
+    public boolean isAvailable (){
+       return isAvailable;
+    }
+
+    public void setAvailable(boolean available){
+        isAvailable = available;
+    }
+
+    public void borrowBook () {
+        if (isAvailable) {
+            isAvailable = false;
+        }
+
+            else{
+            System.out.println("Book is already borrowed");
+
+        }
+    }
+
+    public void returnBook(){
+    isAvailable = true;
+    }
+
+    @Override
+    public String toString(){
+        return title + " by " + author + " (ISBN: " + isbn + ")";
+    }
+}
---
+++ src/main/java/javaclassesandinterfacestask/ClassesAndInterfaceTask_Maryam/User.java
@@ -0,0 +1,56 @@
+package javaclassesandinterfacestask.ClassesAndInterfaceTask_Maryam;
+import java.util.ArrayList;
+public class User {
+
+   private String name;
+   private String libraryId;
+   private ArrayList<Book> booksBorrowed;
+
+    // constructors
+   public User (String name, String libraryId){
+       this.name = name;
+       this.libraryId = libraryId;
+       this.booksBorrowed = new ArrayList<>();
+   }
+
+   //Getters and setters
+
+   public String getName (){
+      return name;
+   }
+   public void setName (String name) {
+      this.name= name;
+   }
+
+   public String getLibraryId(){
+       return libraryId;
+   }
+   public void setLibraryId(String libraryId){
+       this.libraryId = libraryId;
+   }
+   public ArrayList <Book> getBooksBorrowed (){
+       return booksBorrowed;
+   }
+
+// methods
+
+    public void borrowBook(Book book){
+        if (book.isAvailable()) {
+            booksBorrowed.add(book);
+            book.borrowBook();
+        }
+        else {
+            System.out.println("this book is not available");
+        }
+    }
+
+    public void returnBook (Book book) {
+       if (booksBorrowed.contains(book)){
+           booksBorrowed.remove(book);
+           book.returnBook();
+       }
+       else {
+           System.out.println("This book is not borrowed by user");
+       }
+    }
+}
