6c09c30738957a551eb40aff7a3018ccacee7b86
akshathashivananjappa <akshatha.shivananjappa@gymshark.com>
2024-08-19 08:54:42 +0100

    Added the excercise for Variables

---
+++ src/main/java/session1variables/tasks/ShoppingCartAkshatha.java
@@ -0,0 +1,29 @@
+package session1variables.tasks;
+
+public class ShoppingCartAkshatha {
+    public static void main(String[] args) {
+        double itemPriceOne = 3.5;
+        double itemPriceTwo = 4.54;
+        double itemPriceThree = 5.46;
+
+        double salesTaxValue = 0.20;
+
+        int quantityOne = 5;
+        int quantityTwo = 10;
+        int quantityThree = 15;
+
+        double totalItemOne = itemPriceOne * quantityOne;
+        double totalItemTwo = itemPriceTwo * quantityTwo;
+        double totalItemThree = itemPriceThree * quantityThree;
+
+        double itemSubtotal = totalItemOne + totalItemTwo + totalItemThree;
+
+        double tax = salesTaxValue * itemSubtotal;
+
+        double grandTotal = itemSubtotal + tax;
+
+        System.out.println("The subtotal is: " + grandTotal);
+        System.out.println("The tax is: " + tax);
+        System.out.println("The grand total is: " + itemSubtotal);
+    }
+}
