fdb240e36bca8c9b06e499e7dca22fba8127dbb6
akshathashivananjappa <akshatha.shivananjappa@gymshark.com>
2024-09-09 15:57:23 +0100

    Added the exercise for methods

---
+++ src/main/java/session4methods/tasks/AverageGradeCalculatorAkshatha.java
@@ -0,0 +1,17 @@
+package session4methods.tasks;
+
+public class AverageGradeCalculatorAkshatha {
+    public static void main(String[] args) {
+        int[] grades = {6, 7, 8, 7, 8, 9, 9};
+        System.out.println("The average grade is: " + calculateAverageGrade(grades));
+    }
+
+
+    public static double calculateAverageGrade(int[] grades) {
+        int totalGrades = 0;
+        for (int studentGrade : grades) {
+            totalGrades += studentGrade;
+        }
+        return (double) totalGrades / grades.length;
+    }
+}
