f7618e433e2821f01e7ab84729f70e3aec197eff
akeem-watson <akeem.watson@gymshark.com>
2024-09-12 16:29:40 +0100

    Session 4 - string manipulator task complete

---
+++ src/main/java/session5strings/tasks/StringManipulationAndFormattingAkeem.java
@@ -0,0 +1,24 @@
+package session5strings.tasks;
+
+public class StringManipulationAndFormattingAkeem {
+    public static void main(String[] args) {
+
+        //Predefined Data
+        String email = "akeem.watson@gymshark.com";
+        String name = "Akeem Watson";
+
+        //String Methods
+        String extractedUsername = email.substring(0,email.indexOf("@"));
+        String nameUpperCased = name.toUpperCase();
+
+        //String Equality
+        boolean isNameMatched = extractedUsername.equalsIgnoreCase(name.replaceAll(" ", "."));
+
+        //StringBuilder
+        StringBuilder stringBuilder = new StringBuilder();
+        StringBuilder message = stringBuilder.append(email).append("\n").append(extractedUsername).append("\n")
+                .append(nameUpperCased).append("\n").append(isNameMatched);
+
+        System.out.println(message);
+    }
+}
