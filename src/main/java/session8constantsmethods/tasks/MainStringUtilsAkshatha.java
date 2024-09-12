dea513bc13702727bea47d88df2c2384e7eccb09
akshathashivananjappa <akshatha.shivananjappa@gymshark.com>
2024-09-12 08:11:05 +0100

    Added the tests for constructors, constansts and methods for session 5 exercise

---
+++ src/main/java/session8constantsmethods/tasks/MainStringUtilsAkshatha.java
@@ -0,0 +1,11 @@
+package session8constantsmethods.tasks;
+
+
+import static session8constantsmethods.tasks.StringUtilsAkshatha.*;
+
+public class MainStringUtilsAkshatha {
+    public static void main(String[] args) {
+        System.out.println("Coverting String to Uppercase " + toUpperCase(DEFAULT_STRING));
+        System.out.println("Count the value of vowels in the String " + vowelCount(DEFAULT_STRING));
+    }
+}
