c5f938d9c0cd1c54bbd00955e2031f4a3ef8c913
akeem-watson <akeem.watson@gymshark.com>
2024-09-12 09:39:34 +0100

    Session 3 - temp average task complete

---
+++ src/main/java/session3forloops/tasks/TemperatureAverageAkeem.java
@@ -0,0 +1,15 @@
+package session3forloops.tasks;
+
+public class TemperatureAverageAkeem {
+
+    public static void main( String[] args) {
+        int[] temperatures = {15, 10, 25, 16, 19, 12};
+        int sum = 0;
+
+        for (int temperature : temperatures) {
+        sum += temperature;
+        }
+        int averageTemperature = sum / temperatures.length;
+        System.out.println(averageTemperature);
+    }
+}
